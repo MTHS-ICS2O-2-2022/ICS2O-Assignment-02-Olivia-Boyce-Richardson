f9fe0ce67035866fd481a40cde274e44d9b91686
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-03-30 14:37:48 +0000

    html fixes

--- js/script.js
+++ js/script.js
@@ -3,3 +3,19 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+'use strict'
+/**
+ * This function calculates area and perimeter of rectangle.
+ */
+function calculate () {
+  // input
+  const side a = parseInt(document.getElementById('side a length').value)
+  const side b = parseInt(document.getElementById('side b length').value)
+  const base = parseInt(document.getElementById('height').value)
+
+  // process
+  const area = side a length + side b length / 2 * height
+
+  // output
+  document.getElementById('area').innerHTML = 'Area is: ' + area + ' cm²'
+}
